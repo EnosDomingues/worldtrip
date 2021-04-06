ea92f5e298c87021f5b1fdb372dc8285c4a13e7e
Enos Domingues <enosdomingues@gmail.com>
2021-04-06 18:49:38 -0300

    mobile version banner

--- src/components/TravelTypes/TravelType.tsx
+++ src/components/TravelTypes/TravelType.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image, Text, useBreakpointValue } from "@chakra-ui/react";
 
 interface ITravelTypeProps {
   type: string;
@@ -6,10 +6,35 @@ interface ITravelTypeProps {
 }
 
 export function TravelType({type, src}: ITravelTypeProps) {
+  const isMobileScreen = useBreakpointValue({
+    base: true,
+    md: false
+  })
+
+  console.log(isMobileScreen)
+
   return (
-    <Flex direction="column" >
-      <Image src={src} alt="cocktail" w="85px" h="85px" m="0 auto"/>
-      <Text fontWeight="600" lineHeight="9" textAlign="center">{type}</Text>
-    </Flex>
+    <>
+      {isMobileScreen !== undefined && (
+        <Flex direction={isMobileScreen ? 'row' : 'column'} align="center">
+          <Image
+            src={isMobileScreen ? '/ellipse.svg' : src}
+            alt={type}
+            w={isMobileScreen ? '8px' : '85px'}
+            h="85px"
+            m="0 auto"
+          />
+          <Text
+            fontWeight="600"
+            lineHeight="9"
+            textAlign="center"
+            fontSize={["1.125rem", "1.25rem", "1.25rem"]}
+            p="0 8px"
+          >
+            {type}
+          </Text>
+        </Flex>
+      )}
+    </>
   )
 }
--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -1,16 +1,66 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Fragment } from "react";
 import { TravelType } from "./TravelType";
 
 export function TravelTypes() {
+  const isMobileScreen = useBreakpointValue({
+    base: true,
+    md: false
+  })
+
+  const travelTypes = [
+    {src: '/cocktail.svg', type: 'vida noturna'},
+    {src: '/surf.svg', type: 'praia'},
+    {src: '/building.svg', type: 'moderno'},
+    {src: '/museum.svg', type: 'clássico'},
+  ]
+
+
   return (
-    <Flex maxW="1160px" w="100%" h="36" my="24" align="center">
-      <Flex justify="space-between" width="100%">
-        <TravelType src="/cocktail.svg" type="vida noturna"/>
-        <TravelType src="/surf.svg" type="praia"/>
-        <TravelType src="/building.svg" type="moderno"/>
-        <TravelType src="/museum.svg" type="clássico"/>
-        <TravelType src="/earth.svg" type="e mais..."/>
-      </Flex>
-    </Flex>
+    <>
+    {isMobileScreen !== undefined && (
+      <>
+      {isMobileScreen ? (
+        <Flex
+          maxW="1160px"
+          w="100%"
+          h="36"
+          my="24"
+          align="center">
+            <Flex justify="space-between" width="100%" m="0 16px" direction="column" align="center">
+              <Flex w="100%" justify="space-between" p="0 34px">
+                <TravelType src="/earth.svg" type="vida noturna"/>
+                <TravelType src="/earth.svg" type="praia"/>
+              </Flex>
+              <Flex w="100%" justify="space-between" p="0 34px">
+                <TravelType src="/earth.svg" type="moderno"/>
+                <TravelType src="/earth.svg" type="clássico"/>
+              </Flex>
+              <Flex w="100%" justify="center" p="0 34px">
+                <TravelType src="/earth.svg" type="e mais..."/>
+              </Flex>
+            </Flex>
+        </Flex>
+      ):
+      (
+        <Flex
+        maxW="1160px"
+        w="100%"
+        h="36"
+        my="24"
+        align="center">
+          <Flex justify="space-between" width="100%" m="0 16px">
+            {travelTypes.map((type, index) => (
+            <Fragment key={index} >
+              <TravelType src={type.src} type={type.type} />
+            </Fragment>
+            ))}
+            <TravelType src="/earth.svg" type="e mais..."/>
+          </Flex>
+        </Flex>
+      )}
+      </>
+    )}
+    </>
   )
 }
