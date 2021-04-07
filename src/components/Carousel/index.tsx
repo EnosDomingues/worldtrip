deee1c5882548222c5f1bc94a045657da82952fa
Enos Domingues <enosdomingues@gmail.com>
2021-04-07 20:55:19 -0300

    continent

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Image, Text, useBreakpointValue } from '@chakra-ui/react'
 import { Swiper, SwiperSlide }  from 'swiper/react'
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import Link from 'next/link'
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
@@ -65,35 +66,38 @@ export function Carousel() {
             align="center"
             justify="center"
           >
-            <Image src={isMobileVersion ? slide.src : '/swiper-europe-mobile.png'} alt={slide.header} w="100%"/>
-            <Box position="absolute" zIndex="1">
-              <Text
-                as="h1"
-                color="light.50"
-                fontWeight="700"
-                fontSize={["1.5rem",
-                "2rem",
-                "3rem"]}
-                textAlign="center"
-                lineHeight={["1.75rem",
-                "2.5rem",
-                "3.5rem"]}
-              >
-                {slide.header}</Text>
-              <Text
-                as="span"
-                color="light.100"
-                fontWeight="700"
-                fontSize={["0.875rem",
-                "1.125rem",
-                "1.5rem"]}
-                lineHeight={["1.075rem",
-                "1.155rem",
-                "2.95rem"]}
-              >
-                {slide.text}
-              </Text>
-            </Box>
+            <Link href="/continents/1" passHref>
+              <Image src={isMobileVersion ? slide.src : '/swiper-europe-mobile.png'} alt={slide.header} w="100%"/>
+            </Link>
+              <Box position="absolute" zIndex="1">
+                <Text
+                  as="h1"
+                  color="light.50"
+                  fontWeight="700"
+                  fontSize={["1.5rem",
+                  "2rem",
+                  "3rem"]}
+                  textAlign="center"
+                  lineHeight={["1.75rem",
+                  "2.5rem",
+                  "3.5rem"]}
+                >
+                  {slide.header}</Text>
+                <Text
+                  as="span"
+                  color="light.100"
+                  fontWeight="700"
+                  fontSize={["0.875rem",
+                  "1.125rem",
+                  "1.5rem"]}
+                  lineHeight={["1.075rem",
+                  "1.155rem",
+                  "2.95rem"]}
+                >
+                  {slide.text}
+                </Text>
+              </Box>
+            
           </Flex>
         </SwiperSlide>
       ))}
